dda466eb7860209f49f12b0d6e133c6690d9da58
suibiweng <suibi.weng@gmail.com>
2023-09-11 15:46:20 -0600

    whatchdog update

--- FileExportandUploadManager.py
+++ FileExportandUploadManager.py
@@ -1,8 +1,10 @@
 import os
 import time
+import subprocess
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
+import zipfile
+import threading
 
 # import packages
 from google.cloud import storage
@@ -11,6 +13,16 @@ from google.cloud import storage
 import datetime
 import sys
 # 上傳檔名 /位置/
+
+ckptPath=r"/ckpt/last.ckpt"
+yamalpath=""
+
+
+stage=0
+PngFilePath=None
+lastfile=None
+
+
 
 
 class LatestFolderHandler(FileSystemEventHandler):
@@ -25,38 +37,213 @@ class LatestFolderHandler(FileSystemEventHandler):
                 self.latest_folder = folder_path
 
 
-def watch_for_latest_folder(folder_to_watch):
+class PNGHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            return
+        # Check if the created file is a PNG file
+        if event.src_path.lower().endswith(".png"):
+            
+            print(f"upload this: {event.src_path}")
+
+class ModelHandler(FileSystemEventHandler):
+    def __init__(self):
+        super().__init__()
+        self.latest_folder = None
+
+    def on_created(self, event):
+        if event.is_directory:
+            folder_path = event.src_path
+            if not self.latest_folder or os.path.getctime(folder_path) > os.path.getctime(self.latest_folder):
+                self.latest_folder = folder_path
+                zip_folder_contents(self.latest_folder,"obj.zip")
+
+
+
+def zip_folder_contents(folder_path, output_zip):
+    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for foldername, subfolders, filenames in os.walk(folder_path):
+            for filename in filenames:
+                file_path = os.path.join(foldername, filename)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname)
+
+
+
+def on_subprocess_exit(proc):
+    return_code = proc.returncode
+
+    print(f"Subprocess exited with return code {return_code}")
+
+def ZipfolderandUpload(folderPath):
+    print("")
+
+
+
+def ExportTheModel():
+    global yamalpath
+    global ckptPath
+    global stage
+
+    cmd=  f"python launch.py --config {yamalpath} --export --gpu 0 {ckptPath} system.exporter_type=mesh-exporter system.exporter.fmt=obj"
+    try:
+        proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+        on_subprocess_exit(proc)
+        if stdout:
+            print("Subprocess stdout:", stdout.decode())
+        if stderr:
+            print("Subprocess stderr:", stderr.decode())
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+    
+
+
+
+
+
+
+
+
+
+
+
+# def watch_for_latest_folder(folder_to_watch):
+#     global stage
+#     global PngFilePath
+#     global lastfile
+    
+#     #lastfile=None
+#     event_handler = LatestFolderHandler()
+#     png_event = PNGHandler()
+#     observer = Observer()
+#     observer.schedule(event_handler, folder_to_watch, recursive=False)
+#     observer.start()
+
+#     try:
+#         while True: #stage != 3
+#             if event_handler.latest_folder:
+#                 if(lastfile!=event_handler.latest_folder):
+#                     lastfile=event_handler.latest_folder
+#                     print(event_handler.latest_folder)
+                    
+#                     if(stage==0):
+#                         observer.unschedule_all()  # Stop watching the old folder
+#                         observer.schedule(event_handler, lastfile, recursive=False)  # Start watching the new folder
+#                         stage=1
+
+#                     elif(stage==1):
+
+#                         print (os.path.basename(event_handler.latest_folder))
+#                         if( os.path.basename(event_handler.latest_folder)=='save'):
+                            
+#                             PngFilePath=event_handler.latest_folder
+#                             stage=2
+#                             observer.unschedule_all()
+#                             observer.schedule(png_event, PngFilePath, recursive=False) 
+#                             print("changetoSave")
+#                     elif(stage==2):
+#                         pass
+#                     elif(stage==3):
+#                         print("folder to zip and upload it")    
+#                 time.sleep(1)
+#     except KeyboardInterrupt:
+#         observer.stop()
+#     observer.join()
+
+
+def TheModelExporter(folder_to_watch):
+    global stage
+    global PngFilePath
+    global lastfile
+    
+    #lastfile=None
     event_handler = LatestFolderHandler()
+    png_event = PNGHandler()
     observer = Observer()
     observer.schedule(event_handler, folder_to_watch, recursive=False)
     observer.start()
 
     try:
-        while True:
+        while True: #stage != 3
             if event_handler.latest_folder:
-                return event_handler.latest_folder
-            time.sleep(1)
+                if(lastfile!=event_handler.latest_folder):
+                    lastfile=event_handler.latest_folder
+                    print(event_handler.latest_folder)
+                    
+                    if(stage==0):
+                        observer.unschedule_all()  # Stop watching the old folder
+                        observer.schedule(event_handler, lastfile, recursive=False)  # Start watching the new folder
+                        stage=1
+
+                    elif(stage==1):
+
+                        print (os.path.basename(event_handler.latest_folder))
+                        if( os.path.basename(event_handler.latest_folder)=='save'):
+                            
+                            PngFilePath=event_handler.latest_folder
+                            stage=2
+                            observer.unschedule_all()
+                            observer.schedule(png_event, PngFilePath, recursive=False) 
+                            print("changetoSave")
+                    elif(stage==2):
+                        pass
+                    elif(stage==3):
+                        print("folder to zip and upload it")    
+                time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+def PreViewUploader(folder_to_watch):
+    global stage
+    global PngFilePath
+    global lastfile
+    
+    #lastfile=None
+    event_handler = LatestFolderHandler()
+    png_event = PNGHandler()
+    observer = Observer()
+    observer.schedule(event_handler, folder_to_watch, recursive=False)
+    observer.start()
+
+    try:
+        while True: #stage != 3
+            if event_handler.latest_folder:
+                if(lastfile!=event_handler.latest_folder):
+                    lastfile=event_handler.latest_folder
+                    print(event_handler.latest_folder)
+                    
+                    if(stage==0):
+                        observer.unschedule_all()  # Stop watching the old folder
+                        observer.schedule(event_handler, lastfile, recursive=False)  # Start watching the new folder
+                        stage=1
+
+                    elif(stage==1):
+
+                        print (os.path.basename(event_handler.latest_folder))
+                        if( os.path.basename(event_handler.latest_folder)=='save'):
+                            
+                            PngFilePath=event_handler.latest_folder
+                            stage=2
+                            observer.unschedule_all()
+                            observer.schedule(png_event, PngFilePath, recursive=False) 
+                            print("changetoSave")
+                    elif(stage==2):
+                        pass
+                    elif(stage==3):
+                        print("folder to zip and upload it")    
+                time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
     
 
+def upload_file(file_name):
 
-class PNGHandler(FileSystemEventHandler):
-    def on_created(self, event):
-        if event.is_directory:
-            return
-
-        # Check if the created file is a PNG file
-        if event.src_path.lower().endswith(".png"):
-            # Replace this line with your desired action
-            print(f"New PNG file created: {event.src_path}")
-
-
-
-file_name = 'test3.zip'
-bucket_name = 'suibidata'
-def upload_file(bucket_name):
+    bucket_name = 'suibidata'
     # Create a client for interacting with the GCP Storage API, using the ServiceAccount key file
     client = storage.Client.from_service_account_json('studious-bit-398115-420fcd83daa6.json')
     # Create a bucket object
@@ -94,10 +281,25 @@ def get_presigned_url (bucket_name, blob_name) :
     return url
 
 
-folder_to_watch = "/path/to/your/folder"
+
 
 if __name__ == "__main__":
     print('')
+
+    folder_to_watch = "D:\Desktop\RealityEditor\output"
+    # latest_folder_name = watch_for_latest_folder(folder_to_watch)
+
+    PreviewUPdatethread = threading.Thread(target=PreViewUploader, args=(folder_to_watch,))
+    PreviewUPdatethread.start()
+
+
+
+
+
+
+
+   # print(f"Latest created folder name: {latest_folder_name}")
+
     # # The name for the new bucket
     # bucket_name = bucket_name
     # upload_file(bucket_name)
